21e13b4844791b5442e0d1aaca42dc668e676251
shoedop <shoedop.official@gmail.com>
2025-12-30 21:02:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-179594776657147"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body>
         <Header/>
         {children}
